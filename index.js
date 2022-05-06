e276d0e51552f1dd3eaee34af77e6a49efb9b565
Shaminaj Towfika Disha <stowfika88@gmail.com>
2022-05-06 22:23:11 +0600

    get for /items/:id added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
@@ -15,7 +15,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
-        const itemsCollection = client.db("carWarhouse").collection("items");
+        const itemsCollection = client.db("carWarhouse").collection("inventoryItems");
 
         //ITEMS API
         app.get('/items', async (req, res) => {
@@ -23,6 +23,13 @@ async function run() {
             const cursor = itemsCollection.find(query);
             const items = await cursor.toArray();
             res.send(items);
+        });
+
+        app.get('/items/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const item = await itemsCollection.findOne(query);
+            res.send(item);
         });
 
     } finally {
